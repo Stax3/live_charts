47c4b3e06154a18517ef08b80c3ae190958e83de
Stax3 Engineering <engineering@stax3.com>
2024-12-24 21:43:51 +0500

    Initial commit

---
+++ lib/live_charts.ex
@@ -0,0 +1,18 @@
+defmodule LiveCharts do
+  @moduledoc """
+  Documentation for `LiveCharts`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LiveCharts.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/live_charts_test.exs
@@ -0,0 +1,8 @@
+defmodule LiveChartsTest do
+  use ExUnit.Case
+  doctest LiveCharts
+
+  test "greets the world" do
+    assert LiveCharts.hello() == :world
+  end
+end
